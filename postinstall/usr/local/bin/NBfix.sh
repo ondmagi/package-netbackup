3f639593d16c8ede982a793d4574081948cf4856
Andreas Lindh <andreas@superblock.se>
2014-03-10 14:00:46 +0100

    no more file names in script

--- postinstall/usr/local/bin/NBfix.sh
+++ postinstall/usr/local/bin/NBfix.sh
@@ -1,26 +1,12 @@
 #!/bin/bash
-mv /usr/openv/lib/libnbbaseST.so_new /usr/openv/lib/libnbbaseST.so
-mv /usr/openv/lib/libnbbaseST32.so_new /usr/openv/lib/libnbbaseST32.so
-mv /usr/openv/lib/libnbclientST.so_new /usr/openv/lib/libnbclientST.so
-mv /usr/openv/lib/libnbclientST32.so_new /usr/openv/lib/libnbclientST32.so
-mv /usr/openv/lib/libnbcryptowST.so_new /usr/openv/lib/libnbcryptowST.so
-mv /usr/openv/lib/libnbmangle.so_new /usr/openv/lib/libnbmangle.so
-mv /usr/openv/lib/libvrtsLogFormatMsgST.so.3_new /usr/openv/lib/libvrtsLogFormatMsgST.so.3
-mv /usr/openv/lib/libvrtsLogReadST.so.3_new /usr/openv/lib/libvrtsLogReadST.so.3
-mv /usr/openv/lib/libvrtsLogSLST.so.3_new /usr/openv/lib/libvrtsLogSLST.so.3
-mv /usr/openv/lib/libvxACEST.so.6_new /usr/openv/lib/libvxACEST.so.6
-mv /usr/openv/lib/libvxcPBXST.so_new /usr/openv/lib/libvxcPBXST.so
-mv /usr/openv/lib/libvxcryptoST.so.5_new /usr/openv/lib/libvxcryptoST.so.5
-mv /usr/openv/lib/libvxexticuST.so.3_new /usr/openv/lib/libvxexticuST.so.3
-mv /usr/openv/lib/libvxicudataST.so_new /usr/openv/lib/libvxicudataST.so
-mv /usr/openv/lib/libvxicui18nST.so_new /usr/openv/lib/libvxicui18nST.so
-mv /usr/openv/lib/libvxicuucST.so_new /usr/openv/lib/libvxicuucST.so
-mv /usr/openv/lib/libvxlisST.so.3_new /usr/openv/lib/libvxlisST.so.3
-mv /usr/openv/lib/libvxsslST.so.5_new /usr/openv/lib/libvxsslST.so.5
-mv /usr/openv/lib/libvxulST.so.3_new /usr/openv/lib/libvxulST.so.3
-mv /usr/openv/lib/libvxustdioST.so_new /usr/openv/lib/libvxustdioST.so
-mv /usr/openv/netbackup/bin/bpcd_new /usr/openv/netbackup/bin/bpcd
-mv /usr/openv/netbackup/bin/vnetd_new /usr/openv/netbackup/bin/vnetd
+
+for f in `find /usr/openv/lib/ /usr/openv/netbackup/bin/ -name '*_new'`; do
+	old_name=$f
+	new_name=`echo $old_name | sed 's/\(_$\)//g'`
+	echo "Renaming $old_name => $new_name"
+	mv $old_name $new_name
+done
+
 if [ ! -f /usr/openv/netbackup/bp.conf ]; then
 	echo "SERVER = foo.abc.com" >> /usr/openv/netbackup/bp.conf
 	echo "CLIENT_NAME = ${HOSTNAME}" >> /usr/openv/netbackup/bp.conf
